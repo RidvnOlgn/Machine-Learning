270165f78176e5c05ab83ce870de59f48e52f32d
Ridvan Olgun <ridvan.olgun.015@std.yildiz.edu.tr>
2018-07-24 17:55:31 +0300

    Decision tree and random forest added

--- PredictwiththeDecisionTree/dt_and_rd.py
+++ PredictwiththeDecisionTree/dt_and_rd.py
@@ -12,9 +12,21 @@ data = pd.read_csv("salary.csv")
 x = data.iloc[:,1:2].values
 y = data.iloc[:,-1:].values
 
+#decision tree
+
 from sklearn.tree import DecisionTreeRegressor
 r_dt = DecisionTreeRegressor(random_state = 0)
 r_dt.fit(x,y)
 
 plt.scatter(x,y,color="red")
 plt.plot(x,r_dt.predict(x),color="blue")
+plt.show()
+
+#random forest
+
+from sklearn.ensemble import RandomForestRegressor
+rf_reg = RandomForestRegressor(n_estimators = 10,random_state=0)
+rf_reg.fit(x,y)
+
+plt.scatter(x,y,color ="red")
+plt.plot(x,rf_reg.predict(x),color = "blue")
